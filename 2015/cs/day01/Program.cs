c75dcbf1ca348c7d5a622a28aa436ae30f72b298
Kashif Soofi <kashif.soofi+git@gmail.com>
2022-12-03 21:39:36 +0000

    Advent of Code 2015 Day 01 C# solution

---
+++ 2015/cs/day01/Program.cs
@@ -0,0 +1,51 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine($"Current Floor: {part1_GetCurrentFloor()}, Position: {part2_GetPosition()}");
+
+int part1_GetCurrentFloor()
+{
+    var currentFloor = 0;
+    using (var reader = new StreamReader("../../inputs/day01.txt"))
+    {
+        while (!reader.EndOfStream)
+        {
+            var c = (char)reader.Read();
+            if (c == '(')
+            {
+                currentFloor++;
+            }
+            else if (c == ')')
+            {
+                currentFloor--;
+            }
+        }
+    }
+    return currentFloor;
+}
+
+int part2_GetPosition()
+{
+    var position = 1;
+    var currentFloor = 0;
+    using (var reader = new StreamReader("../../inputs/day01.txt"))
+    {
+        while (!reader.EndOfStream)
+        {
+            var c = (char)reader.Read();
+            if (c == '(')
+            {
+                currentFloor++;
+            }
+            else if (c == ')')
+            {
+                currentFloor--;
+            }
+
+            if (currentFloor == -1)
+            {
+                break;
+            }
+            position++;
+        }
+    }
+    return position;
+}
